a9566712fc7e44df938e7dd7a9f7c5a3221e81be
Mohamed1242 <mohalaa.124@gmail.com>
2024-08-07 18:35:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import './main.css'
+import axios from "axios"
+function App(){
+const[city,setCity]=useState("")
+const[temp,setTemp]=useState("")
+const[wind,setwind]=useState("")
+const[sky,setSky]=useState("")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getWeather() {
+if(city!==""){
+  try{
+    let response=await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}%2Cegypt&appid=30240420ce3b384343d60d34912ab7db`)
+    console.log(response.data)
+    setTemp(response.data.main.temp)
+    setwind(response.data.wind.speed)
+    setSky(response.data.weather[0].description)
+  }catch(err){
+    if(err.response.status===404){
+      window.alert("country not found")
+
+    }
+    console.log(err)
+  }
+}else{
+  window.alert("enter city first")
 }
-
-export default App;
+}
+function calc(temp){
+  if(temp!==""){
+    return temp - 273.15
+  }
+}
+let ftemp=calc(temp)
+console.log(ftemp)
+  return(
+    <div className="main">
+    <div className="top">
+      <input
+      type="text"
+      onChange={(e)=>setCity(e.target.value)}
+      ></input>
+      <button onClick={getWeather}>get weather</button>
+    </div>
+    <div className="bottom">
+      <div>
+        <h3>temp</h3>
+        {temp!==""?<h1>{ftemp.toFixed(2)}</h1>:<h1>enter city</h1>}
+      </div>
+      <div>
+        <h3>wind</h3>
+        {wind!==""?<h1>{wind}</h1>:<h1>enter city</h1>}      </div>
+      <div>
+        <h3>cloud</h3>
+        {sky!==""?<h1>{sky}</h1>:<h1>enter city</h1>}
+      </div>
+    </div>
+    </div>
+  )
+}
+export default App
